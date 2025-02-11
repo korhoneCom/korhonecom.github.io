3b1ecd32710aaa045d4c6205cab277e472676011
korhoneCom <103757909+korhoneCom@users.noreply.github.com>
2025-02-11 15:21:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import kaplay from "https://unpkg.com/kaplay@3001/dist/kaplay.mjs";
 kaplay();
 loadSprite("dracula", "dracula.png");
 
-// add something to screen
 add([sprite("dracula"), pos(80, 40)]);
 add([
   text("KorhoneCom KAPLAY testing"), 
